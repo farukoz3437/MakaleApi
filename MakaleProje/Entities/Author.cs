2d06a1902b320b2dd0b7b80cdcb6cbc70165db6a
faruk öz <o.faruk.oz@hotmail.com>
2020-10-10 14:29:02 +0300

    Entity ler oluşturuldu

---
+++ MakaleProje/Entities/Author.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Entities
+{
+    public class Author:IEntity
+    {
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+    }
+}
---
+++ MakaleProje/Entities/Concrete/Article.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Article:IEntity
+    {
+        public int ArticleId { get; set; }
+        public string ArticleName { get; set; }
+        public string ArticleContent { get; set; }
+        public DateTime ArticleReleaseDate { get; set; }
+        public DateTime ArticleCreateDate { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
