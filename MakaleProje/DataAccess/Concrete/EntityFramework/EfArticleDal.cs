afdbf12006bd3f331cd1a6fe1a79e7c663d280e1
faruk öz <o.faruk.oz@hotmail.com>
2020-10-10 15:47:43 +0300

    Core katmanı altında RepositoryPattern deseniyle Ekleme,Silme,Güncelleme, Listeleme, filteye göre nesne getirme metodları yazılmıştır.

--- MakaleProje/DataAccess/Concrete/EntityFramework/EfArticleDal.cs
+++ MakaleProje/DataAccess/Concrete/EntityFramework/EfArticleDal.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Core.DataAcccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfArticleDal
+    public class EfArticleDal:EfEntityRepositoryBase<Article,ArticleContext>,IArticleDal
     {
     }
 }
