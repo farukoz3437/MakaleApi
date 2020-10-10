8ab1640fe5febeb03a2be4d6c81253c1ab301cfb
faruk öz <o.faruk.oz@hotmail.com>
2020-10-10 15:01:31 +0300

    DataAcces katmanında, EntityFramework kütüphanesi yüklendi ve Context nesnesi oluşturuldu. Dbsetler oluşturuldu

---
+++ MakaleProje/DataAccess/Concrete/EntityFramework/Contexts/ArticleContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class ArticleContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=..........");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
